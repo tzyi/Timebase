7f1796fe6dc2bb2e5f7291f3be99153e0ec57295
tzyi <cabie8399@gmail.com>
2026-07-21 10:31:09 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '192.168.10.109'],
 };
 
 export default nextConfig;
